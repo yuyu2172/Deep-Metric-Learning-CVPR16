19c2d50235e24526ae8ee563c1f3bfb418683c39
Yusuke Niitani <yuyuniitani@gmail.com>
2017-08-08 19:51:33 +0900

    paths

--- code/config.m
+++ code/config.m
@@ -4,9 +4,13 @@ conf = struct();
 
 %% Directories
 
-conf.root_path = '/scail/scratch/group/cvgl/hsong/Deep-Lifting-for-Metric-Learning-CVPR/code/ebay/';
-conf.cache_path = '/scail/scratch/group/cvgl/hsong/Deep-Lifting-for-Metric-Learning-CVPR/code/ebay/cache';
-conf.image_path = '/cvgl/group/Ebay_Dataset/';
+
+conf.root_path = '/home/leus/playground/Deep-Metric-Learning-CVPR16/';
+conf.image_path = '/home/leus/.chainer/dataset/pfnet/chainercv/online_products/Stanford_Online_Products/';
+conf.cache_path = '/home/leus/playground/Stanford_Online_Products/';
+% conf.root_path = '/scail/scratch/group/cvgl/hsong/Deep-Lifting-for-Metric-Learning-CVPR/code/ebay/';
+% conf.cache_path = '/scail/scratch/group/cvgl/hsong/Deep-Lifting-for-Metric-Learning-CVPR/code/ebay/cache';
+% conf.image_path = '/cvgl/group/Ebay_Dataset/';
 
 %% Training parameters
 conf.preprocessing.crop_padding = 15;
@@ -14,7 +18,9 @@ conf.preprocessing.square_size = 256;
 conf.preprocessing.num_to_load = 255;
 conf.preprocessed_image_file = [conf.cache_path, '/training_images.mat'];
             
-path_triplet = '/cvgl/u/hsong/ebay/cache';
+% path_triplet = '/cvgl/u/hsong/ebay/cache';
+path_triplet = '/home/leus/playground/Stanford_Online_Products_triplet/';
+
 
 % for multilabel pairs batchsize = 128
 conf.training_set_path_multilabel_m128 = [path_triplet, '/training_set_cars196_multilabel_m128.lmdb'];
